0b9f512a1ac4bcfed34679092b3f46a752882af2
Rajesh Kasani <rks9kor@bosch.com>
2020-01-21 11:57:32 +0530

    Project entire structure till web Api created for development

---
+++ PetPeers.API.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PetPeers.API;
+using PetPeers.API.Controllers;
+
+namespace PetPeers.API.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
